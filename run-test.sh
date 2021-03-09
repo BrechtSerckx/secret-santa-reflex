a66e778911690d9120b431e15fb568d105be2267
Brecht Serckx <services+git@brechtserckx.be>
2021-03-09 18:46:06 +0100

    Update test script

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env nix-shell
 #! nix-shell  -i bash -A shells.ghc
 
+FLAGS=()
+
+# FLAGS+=(--email-backend None)
+# FLAGS+=(--email-backend GMail)
+FLAGS+=(--email-backend SES)
+
 # run with configuration in a `.env` file, that for obvious reasons is not
 # included in here
-env $(cat .env | xargs) cabal run backend -- --email-backend GMail
+env $(cat .env | xargs) cabal run backend -- "${FLAGS[@]}"
