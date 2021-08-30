abdf0e11e5d3925b82b07162f35ca6d571e86e47
Brecht Serckx <services+git@brechtserckx.be>
2021-08-30 16:07:58 +0200

    Log messages

--- backend/src/SecretSanta.hs
+++ backend/src/SecretSanta.hs
@@ -6,8 +6,13 @@ import           Prelude                 hiding ( log )
 
 import           Polysemy
 import           Polysemy.Error
+import           Polysemy.Operators
 
 import           Colog.Core
+import           Colog.Message                  ( Message
+                                                , Msg(..)
+                                                , fmtMessage
+                                                )
 import           Colog.Polysemy
 
 import           Data.Error
@@ -19,7 +24,8 @@ import           SecretSanta.Opts
 import           SecretSanta.Server
 
 runSecretSanta :: IO ()
-runSecretSanta = runFinal . embedToFinal . runLogAction logTextStdout $ do
+runSecretSanta = runFinal . embedToFinal . runLogAction logMessageStdout $ do
+  logInfo "Welcome to Secret Santa"
   res <-
     runError @ExtError
     . fromExceptionSemVia @SomeException (mkError . T.pack . displayException)
@@ -32,11 +38,28 @@ runSecretSanta = runFinal . embedToFinal . runLogAction logTextStdout $ do
           CreateDB CreateDBOpts {..}        -> case cdbDatabaseBackend of
             AnyDatabaseBackend (Proxy :: Proxy db) opts ->
               runDBConfig @db opts $ createDB @db
+  logInfo "Have a nice day!"
   case res of
     Left e -> do
-      log $ errMessage e
-      embed . die $ errMessage e
+      logError $ errMessage e
+      embed exitFailure
     Right () -> embed exitSuccess
 
-logTextStdout :: MonadIO m => LogAction m Text
-logTextStdout = contramap T.unpack logStringStdout
+logMessageStdout :: MonadIO m => LogAction m Message
+logMessageStdout = contramap (T.unpack . fmtMessage) logStringStdout
+
+logMsg :: (Member (Log Message) r, HasCallStack) => Severity -> Text -> r @> ()
+logMsg severity msg =
+  log Msg { msgSeverity = severity, msgStack = callStack, msgText = msg }
+
+logDebug :: (Member (Log Message) r, HasCallStack) => Text -> r @> ()
+logDebug = logMsg Debug
+
+logInfo :: (Member (Log Message) r, HasCallStack) => Text -> r @> ()
+logInfo = logMsg Info
+
+logWarning :: (Member (Log Message) r, HasCallStack) => Text -> r @> ()
+logWarning = logMsg Warning
+
+logError :: (Member (Log Message) r, HasCallStack) => Text -> r @> ()
+logError = logMsg Error
