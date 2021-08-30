efca3fec3c847830c38dfd683f239d9d3d6ab6e9
Brecht Serckx <services+git@brechtserckx.be>
2021-08-30 13:48:48 +0200

    Log text messages at startup

--- backend/src/SecretSanta.hs
+++ backend/src/SecretSanta.hs
@@ -2,10 +2,16 @@ module SecretSanta
   ( runSecretSanta
   ) where
 
+import           Prelude                 hiding ( log )
+
 import           Polysemy
 import           Polysemy.Error
 
+import           Colog.Core
+import           Colog.Polysemy
+
 import           Data.Error
+import           Data.Functor.Contravariant
 import qualified Data.Text                     as T
 
 import           SecretSanta.Backend.KVStore
@@ -13,10 +19,11 @@ import           SecretSanta.Opts
 import           SecretSanta.Server
 
 runSecretSanta :: IO ()
-runSecretSanta = runFinal . embedToFinal $ do
+runSecretSanta = runFinal . embedToFinal . runLogAction logTextStdout $ do
   res <-
     runError @ExtError
     . fromExceptionSemVia @SomeException (mkError . T.pack . displayException)
+    . raise @(Error ExtError)
     . raise
     $ do
         cmd <- embed parseCmd
@@ -26,6 +33,10 @@ runSecretSanta = runFinal . embedToFinal $ do
             AnyDatabaseBackend (Proxy :: Proxy db) opts ->
               runDBConfig @db opts $ createDB @db
   case res of
-    Left  e  -> embed . die $ errMessage e
+    Left e -> do
+      log $ errMessage e
+      embed . die $ errMessage e
     Right () -> embed exitSuccess
 
+logTextStdout :: MonadIO m => LogAction m Text
+logTextStdout = contramap T.unpack logStringStdout
